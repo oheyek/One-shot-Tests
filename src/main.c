9863f6efb9210fa8ee1c95bc604bd757ea066e0d
oheyek <oheyek@proton.me>
2024-11-18 11:56:25 +0100

    + Handling user input events from the keyboard

--- src/main.c
+++ src/main.c
@@ -6,6 +6,30 @@ static GtkWidget *question_label;
 static GtkWidget *answer_buttons[MAX_ANSWERS];
 static QuestionList *question_list;
 static int current_question = 0;
+static GtkApplication *app_ref; // Reference to the application object
+
+// Helper function to set button labels with key number
+static void set_button_label_with_key(GtkButton *button, const char *text, int key_number) {
+    char label_with_key[256];
+    snprintf(label_with_key, sizeof(label_with_key), "%d: %s", key_number + 1, text);
+    gtk_button_set_label(button, label_with_key);
+}
+
+// Function to load the next question
+static void load_next_question() {
+    if (current_question < question_list->size) {
+        // Update the question label with the new question
+        gtk_label_set_text(GTK_LABEL(question_label), question_list->questions[current_question]->question_text);
+        for (int i = 0; i < MAX_ANSWERS; i++) {
+            set_button_label_with_key(GTK_BUTTON(answer_buttons[i]),
+                                      question_list->questions[current_question]->answers[i], i);
+        }
+    } else {
+        // End of the quiz
+        g_print("Quiz finished!\n");
+        g_application_quit(G_APPLICATION(app_ref)); // Correctly quit the application
+    }
+}
 
 // Callback function for when an answer button is clicked
 static void on_answer_clicked(GtkButton *button, gpointer user_data) {
@@ -21,22 +45,36 @@ static void on_answer_clicked(GtkButton *button, gpointer user_data) {
 
     // Move to the next question
     current_question++;
-    if (current_question < question_list->size) {
-        // Update the question label and answer buttons with the next question
-        gtk_label_set_text(GTK_LABEL(question_label), question_list->questions[current_question]->question_text);
-        for (int i = 0; i < MAX_ANSWERS; i++) {
-            gtk_button_set_label(GTK_BUTTON(answer_buttons[i]), question_list->questions[current_question]->answers[i]);
+    load_next_question();
+}
+
+// Function to handle keyboard input
+static gboolean on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data) {
+    // Check which key was pressed (keys 1-4)
+    if (event->keyval >= GDK_KEY_1 && event->keyval <= GDK_KEY_4) {
+        int answer_index = event->keyval - GDK_KEY_1;
+        Question *q = question_list->questions[current_question];
+
+        // Check if the selected answer is correct
+        if (answer_index < MAX_ANSWERS) {
+            if (answer_index == q->correct_answer) {
+                g_print("Correct!\n");
+            } else {
+                g_print("Wrong.\n");
+            }
+
+            // Move to the next question
+            current_question++;
+            load_next_question();
         }
-    } else {
-        // End of the quiz
-        g_print("Quiz finished!\n");
-        GtkWidget *window = gtk_widget_get_toplevel(GTK_WIDGET(button));
-        gtk_widget_destroy(window);
     }
+    return FALSE; // Continue processing other events
 }
 
 // Function to set up the main application window and widgets
 static void activate(GtkApplication *app, gpointer user_data) {
+    app_ref = app; // Store the application reference for later use
+
     GtkWidget *window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "One-Shot Tests");
     gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
@@ -54,10 +92,15 @@ static void activate(GtkApplication *app, gpointer user_data) {
 
     // Create and add the answer buttons
     for (int i = 0; i < MAX_ANSWERS; i++) {
-        answer_buttons[i] = gtk_button_new_with_label(question_list->questions[current_question]->answers[i]);
+        answer_buttons[i] = gtk_button_new();
+        set_button_label_with_key(GTK_BUTTON(answer_buttons[i]), question_list->questions[current_question]->answers[i],
+                                  i);
         g_signal_connect(answer_buttons[i], "clicked", G_CALLBACK(on_answer_clicked), GINT_TO_POINTER(i));
         gtk_box_pack_start(GTK_BOX(button_box), answer_buttons[i], TRUE, TRUE, 0);
     }
+
+    // Connect the key press event to the window
+    g_signal_connect(window, "key-press-event", G_CALLBACK(on_key_press), NULL);
 
     gtk_widget_show_all(window);
 }
